15e883d30a9a0bccf5348456b62346cb55a7563b
Gab Soulavy <g.soulavy@gmail.com>
2018-04-15 12:57:06 +0100

    Add yarn instead of npm

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         return (<div>
-            <h1>React App!</h1>
+            <h1>React App with Yarn!</h1>
         </div>);
     }
 }
