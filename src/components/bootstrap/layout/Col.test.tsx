83c6a87fea61651c25100e31204e5db0eab2487b
Gab Soulavy <g.soulavy@gmail.com>
2018-04-29 20:59:07 +0100

    Add first unit tests for Col

---
+++ src/components/bootstrap/layout/Col.test.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import * as ReactDOM from 'react-dom';
+import { shallow } from "enzyme";
+import Col from "./Col";
+
+describe("<Col />", () => {
+
+    it("No params -> class='col'", () => {
+        const component = shallow(<Col>hi</Col>);
+        expect(true).toBe(component.hasClass("col"));
+    });
+
+    it("col='1' -> class='col-1'", () => {
+        const component = shallow(<Col col={1}>hi</Col>);
+        expect(true).toBe(component.hasClass("col-1"));
+    });
+
+    it("col='auto' -> class='col-auto'", () => {
+        const component = shallow(<Col col="auto">hi</Col>);
+        expect(true).toBe(component.hasClass("col-auto"));
+    });
+
+    it("Col test", () => {
+        expect(true).toBe(true);
+    });
+});
+
