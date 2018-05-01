94b8411d40a4df1b2698cced6558d4efa120aea9
Gab Soulavy <g.soulavy@gmail.com>
2018-05-01 06:38:17 +0100

    add routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,12 +9,26 @@ class App extends Component<{}, {}> {
         return (
             <Router>
                 <div>
+                    <div>Before links</div>
+                    <Link to="/home">Home</Link>
                     <Link to="/jumbotron">Jumbi</Link>
+                    <Link to={`/tosome/${5}`}>5</Link>
+                    <div>After links</div>
                     <Route path="/jumbotron" component={Jumbo} />
+                    <Route path="/tosome/:id" component={Some} />
+                    <Route path="/home" render={() => <div>Home</div>}/>
+                    <div>After routes</div>
                 </div>
             </Router>
         );
     }
+}
+
+const Some = ({ match }) => {
+    return (
+        <div>
+            The id is: {match.params.id}
+        </div>);
 }
 
 export default App;
