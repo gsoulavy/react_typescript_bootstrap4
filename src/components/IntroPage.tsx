cb437c2f2b07c718eb9fd978ba69d00ab2693a5d
Gab Soulavy <g.soulavy@gmail.com>
2018-05-06 16:47:19 +0100

    Add code snippet to Intro

--- src/components/IntroPage.tsx
+++ src/components/IntroPage.tsx
@@ -8,6 +8,18 @@ const IntroPage: React.StatelessComponent<{}> = () => {
             <Jumbotron>
                 <h1>Bootstrap 4 components with React and Typescript</h1>
             </Jumbotron>
+            <Container>
+                <code className="language-html" data-lang="html">
+                    <div>
+                        <span className="nt">&lt;div</span>
+                        <span className="na">class=</span>
+                        <span className="s">"col"</span>
+                        <span className="nt">&gt;</span>
+                        One of three columns
+                    <span className="nt">&lt;/div&gt;</span>
+                    </div>
+                </code>
+            </Container>
         </Container>
     );
 };
